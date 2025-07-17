bf33ab9f2249b84eb145ec690f8a12e48f871012
Dvlpr Prgmr <dvlpr.prgmr@gmail.com>
2025-07-18 01:19:21 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+console.log('🎯 Netlify Build ENV:', process.env.VITE_GEMINI_API_KEY);
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
 })
